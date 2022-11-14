7c824f4b8d7cf54567548a05b308c85adb275b7a
Hp <Hp@DESKTOP-NVV8KB0>
2022-11-13 20:58:11 -0500

    Agregar archivos de proyecto.

---
+++ capitulo 2 ejercicio 7/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ejercicio7
+{
+    public class leche
+    {
+
+        public static void Main()
+        {
+            double lpg = 3.785;
+            double LV, PG, CG, VV;
+
+            Console.WriteLine("por favor digite los litros de la venta");
+            LV = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("por favor digite el precio del galon");
+            PG = double.Parse(Console.ReadLine());
+            CG = LV / lpg;
+            VV = CG * PG;
+            Console.WriteLine($"el valor venta es {VV}");
+            Console.WriteLine($"cantidad de galones {CG}");
+        }
+    }
+}
